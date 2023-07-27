2f98f6e892fa24e466e6562bd359d3692bd4db11
Jonas Madsen <jonasmadsen96dk@gmail.com>
2023-07-28 00:02:55 +0200

    Fetch arguments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "Deduper.h"
 
-int main(){
-    std::string input = "../Google_tests/test_data/test.wmv";
-    std::string res = "../res.csv";
-    Deduper d(input,  res, 512,6,16,1024*1024);
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cout << "Usage: " << argv[0] << " <input_file> <output_file>" << std::endl;
+        return 1;
+    }
+
+    std::string input = argv[1];
+    std::string res = argv[2];
+    Deduper d(input, res, 512, 12, 16, 1024*1024);
     d.silent = false;
     d.dedupe();
+    d.write_to_file();
+    std::cout << "Finished";
+    return 0;
 }
